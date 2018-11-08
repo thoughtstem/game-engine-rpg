9e9ddca3c4d1161d5164d2d223828ddbda6cd900
srfoster <stephen@thoughtstem.com>
2018-11-08 13:54:31 -0800

    fixing build issue

--- info.rkt
+++ info.rkt
@@ -2,8 +2,14 @@
 
 (define version "0.0.1")
 
+;Causes some kind of build issue:
+;  cannot require racket/gui/base a second time in the same process
+;Commenting out to fix.
+;(define scribblings '(("docs/game-engine-rpg.scrbl" ())))
 
-(define scribblings '(("docs/game-engine-rpg.scrbl" ())))
+(define compile-omit-paths '(
+  "docs"
+))
 
 (define deps '(
   "https://github.com/thoughtstem/game-engine.git"
